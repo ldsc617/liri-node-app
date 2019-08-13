8306eef1d78d7bc47ffb72ad99c683c8763576eb
ldsc617 <ldsc617@gmail.com>
2019-08-12 18:15:33 -0700

    redoing

--- liri.js
+++ liri.js
@@ -5,13 +5,26 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-var moment = require('moment');
 var axios = require('axios');
+var moment = require('moment');
+
 var fs = require('fs');
 
 //user input vars
 var command = process.argv[2];
 var input = process.argv[3];
+
+
+
+if (process.argv.length > 3){
+
+    for(var i = 3; i<process.argv.length; i++){
+        command += " " + process.argv[i];
+        
+    }
+    command = command.trim();
+}
+
 
 switch (command) {
     case "concert-this":
@@ -30,3 +43,40 @@ switch (command) {
         break;
 };
 
+function concertFunc(artist){
+    
+    var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+   
+
+    axios.get(url).then(function(response){
+        console.log(artist);
+        for(var i =0; i< response.data.length; i++){
+            console.log("Name of venue: " +response.data[i].venue.name);
+            console.log("Located in " +response.data[i].venue.city + " " + response.data[i].venue.country);
+            var date = moment(response.data[i].datetime).format("MM/DD/YYYY HH:mm:ss");
+            console.log("Date of concert: " +date + "\n");
+        }
+
+    });
+
+}
+
+function spotifyFunc(songName){
+    if(songName == ""){
+        songName = "Ace of Base";
+    }
+
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        
+
+      console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+      console.log("Song name: " + data.tracks.items[0].name);
+      console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+      console.log("Album: " + data.tracks.items[0].album.name);
+  
+      });
+
+}
